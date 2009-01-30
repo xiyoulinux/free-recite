c16806ed3cba74aa9a72df97b4ab73e9462ea1ad
Kermit.Mei <Kermit.Mei@e6ba875e-b557-11dd-801b-97293e7877ac>
2009-01-30 08:26:22 +0000

    V1.0 Beta 3

--- src/core/ConfigHolder.h
+++ src/core/ConfigHolder.h
@@ -13,63 +13,39 @@ class ConfigHolder
 public:
   ConfigHolder();
   void setRootDir(const char *dir);
-  void setTasksDir(const char *dir);
-  void setMgrFile(const char *filePath);
-  void setDictFile(const char *filePath);
-  void setKeyFile(const char *filePath);
-  void setOutKeyFile(const char *filePath);  
-  void setExportPath(const char *dir);
 
-  const std::string &rootDir() const;
-  const std::string &tasksDir() const;
-  const std::string &mgrFile() const;
-  const std::string &dictFile() const;
-  const std::string &keyFile() const;
-  const std::string &outKeyFile() const;
-  const std::string &exportPath() const;
+  const std::string rootDir() const;
+  const std::string tasksDir() const;
+  const std::string mgrFile() const;
+  const std::string dictFile() const;
+  const std::string keyFile() const;
 private:
   std::string rootDirectory;
-  std::string tasksDirectory;
-  std::string managerFile;
-  std::string dictionaryFile;
-  std::string keystoneFile;
-  std::string outKeystoneFile;
-  std::string outExportPath;
 };
 
 inline
-const std::string &ConfigHolder::rootDir() const {
+const std::string ConfigHolder::rootDir() const {
   return rootDirectory;
 }
 
 inline
-const std::string &ConfigHolder::tasksDir() const {
-  return tasksDirectory;
+const std::string ConfigHolder::tasksDir() const {
+  return rootDirectory + "/tasks/";
 }
  
 inline
-const std::string &ConfigHolder::mgrFile() const {
-  return managerFile;
+const std::string ConfigHolder::mgrFile() const {
+  return rootDirectory + "/freeRecite.mgr";
 }
 
 inline
-const std::string &ConfigHolder::dictFile() const {
-  return dictionaryFile;
+const std::string ConfigHolder::dictFile() const {
+  return rootDirectory + "/freeRecite.dict";
 }
 
 inline
-const std::string &ConfigHolder::keyFile() const {
-  return keystoneFile;
-}
-
-inline
-const std::string &ConfigHolder::outKeyFile() const {
-  return outKeystoneFile;
-}
-
-inline
-const std::string &ConfigHolder::exportPath() const {
-  return outExportPath;
+const std::string ConfigHolder::keyFile() const {
+  return rootDirectory + "keystone.txt";
 }
 
 }//End of namespace freeRecite.
